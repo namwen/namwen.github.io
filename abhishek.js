94c544d7d9e493a1589e72b673cde9687bd23066
Shreyas Jaganmohan <shreyas.jaganmohan@gmail.com>
2014-12-17 18:53:56 -0500

    sound

--- abhishek.js
+++ abhishek.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
     var ww = $(window).width();
     var wh = $(window).height();
     $(".abhishek").each(function(i){
-        var rotationNum=Math.round((Math.random()*360)+1);
+        var rotationNum=Math.round((Math.random()*720)+1);
         var rotation="rotate("+rotationNum+"deg)";
         var posx = Math.round(Math.random() * ww)-20;
         var posy = Math.round(Math.random() * wh)-20;
@@ -21,12 +21,16 @@ $(document).ready(function(){
     $('.abhishek').click(function(){
         $('.abhishek').animate({  borderSpacing: -1080 }, {
             step: function(now,fx) {
-            $(this).css('-webkit-transform','rotate('+now+'deg)'); 
+            $(this).css('-webkit-transform','rotate('+now+'deg)');
             $(this).css('-moz-transform','rotate('+now+'deg)');
             $(this).css('transform','rotate('+now+'deg)');
             },
             duration:'slow'
         },'linear');
+        play()
     });
-
+    function play(){
+         var audio = document.getElementById("audio");
+         audio.play();
+    }
 });
